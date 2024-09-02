42db2dc79f074c3f6b09f16416c2841d8b1f6d7d
ram6013 <ramongarciaprado2002@gmail.com>
2024-09-03 01:40:20 +0200

    algun desperfecto

--- src/App.js
+++ src/App.js
@@ -5,37 +5,39 @@ import { motion } from "framer-motion";
 
 function App() {
   const [activeTab, setActiveTab] = useState("null")
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
   const [IsNight, SetNight] = useState("day")
 
-  const handleMouseEnter= (tab) => {
+  const handleMouseEnter = (tab) => {
     setActiveTab(tab);
     setIsVisible(false);
   }
 
-  const handleMouseLeave = (tab) =>{
-      setActiveTab(tab);
-      setIsVisible(true)
+  const handleMouseLeave = (tab) => {
+    setActiveTab(tab);
+    setIsVisible(true)
   }
 
-  const handleIsNight = ()=>{
+  const handleIsNight = () => {
     SetNight(IsNight === "night" ? "day" : "night");
     console.log(IsNight)
   }
   return (
-    <div className={` h-screen w-full  ${IsNight === "day" ? "bg-white" : "bg-black"}`}>
-      <motion.header className={` ${IsNight === "day" ? "bg-gray-200" : "bg-gray-700"}  flex justify-start items-center p-4`} onMouseLeave={()=>handleMouseLeave("null")}>
-        <div className='p-4 flex justify-start z-10'>
-        <motion.button whileHover={{ scale: 1.5}} onMouseEnter={()=>handleMouseEnter("menu")} ><img src='barra-de-menus.png' width={50}></img></motion.button>
-        </div>
-        {activeTab === "menu" && <motion.div className='absolute w-full flex items-center justify-around'><Barra/></motion.div>}
-        <div className='bg-red-300 absolute w-full flex items-center justify-center'>
-        <motion.h1 className={`pointer-events-none ${isVisible ? 'opacity-100' : 'opacity-0'} ${isVisible ? '' : 'pointer-events-none absolute top-0 left-0 right-0 bottom-0'} text-4xl`}>RGP Store</motion.h1>
-        </div>
-        <div className='flex justify-end w-full'>
-        <motion.button className={`z-10 ${isVisible ? 'opacity-100' : 'opacity-0'} `} whileHover={{scale: 1.5}} onClick={handleIsNight}><img className='h-10' src='luna.png' alt='Modo noche'></img></motion.button>
-        </div>
+    <div className={`h-screen w-full  ${IsNight === "day" ? "bg-white" : "bg-black"}`}>
+      {/* Header */}
+
+      <motion.header className={`eoverflow-hidden relative ${IsNight === "day" ? "bg-gray-200" : "bg-gray-700"}  flex justify-between items-center p-4`} onMouseLeave={() => handleMouseLeave("null")}>
+        <motion.button className='z-30' whileHover={{ scale: 1.2 }} transition={{duration: 0.2}} onMouseEnter={() => handleMouseEnter("menu")} ><img src='barra-de-menus.png' width={50}></img></motion.button>
+        {activeTab === "menu" && <motion.div className=' w-full z-20 flex items-center justify-between absolute'><Barra /></motion.div>}
+        <motion.h1 whileHover={{scale: 1.2}} transition={{duration: 0.2}} className={`${isVisible ? 'opacity-100' : 'opacity-0'} text-4xl`}>RGP Store</motion.h1>
+        <motion.button className={` ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'} `} whileHover={{ scale: 1.3 }} onClick={handleIsNight}><img className='h-10' src='luna.png' alt='Modo noche'></img></motion.button>
       </motion.header>
+
+      {/* Body */}
+      <body>
+        
+      </body>
+
     </div>
   );
 }
